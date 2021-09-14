a47d2f703eea04af4f8b1fed5a2de8904c97e7f8
ipsvrtm <88458824+ipsvrtm@users.noreply.github.com>
2021-09-14 19:39:14 +0300

    Update cheklnatv.php

--- cheklnatv.php
+++ cheklnatv.php
@@ -1,1 +1,9 @@
+<?php
+  $istemci = curl_init();
+  curl_setopt($istemci, CURLOPT_URL, 'https://www.atv.com.tr/canli-yayin');
+  curl_setopt($istemci, CURLOPT_RETURNTRANSFER, 1);
+  $ham_veri = curl_exec($istemci);
+  preg_match_all('#<div data-fb-disable-controls=(.*?)</div>#si'', $ham_veri, $satirlar);
+  echo $satirlar;
+?>
 
